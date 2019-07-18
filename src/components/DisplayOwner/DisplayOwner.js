744ab522c277977a6c06b7988ebcb065ebafd599
Weizhen Wang <wangwzhen@gmail.com>
2019-07-18 14:10:21 -0500

    create table and get owner name

---
+++ src/components/DisplayOwner/DisplayOwner.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+//material ui
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+class DisplayOwner extends Component {
+
+    state ={
+        owners: '',
+    }
+
+    componentDidMount() {
+      axios.get('/api/owners')
+      .then(
+        response => {
+          console.log(response.data);
+          this.setState({
+              owners: response.data,
+          })
+        }
+      )
+    }
+
+  render() {
+    return (
+      <div className="App">
+        {JSON.stringify(this.state.owners)}
+        <h2>Owners</h2>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Number of Pets</TableCell>
+                    <TableCell>Actions</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {this.state.owners && this.state.owners.map(owner => 
+                    <TableRow key={owner.id}>
+                        <TableCell>{owner.name}</TableCell>
+                        <TableCell>1</TableCell>
+                        <TableCell><button>Delete</button></TableCell>
+                    </TableRow>
+                )}
+            </TableBody>
+        </Table>
+      </div>
+    );
+  }
+}
+
+export default DisplayOwner;
